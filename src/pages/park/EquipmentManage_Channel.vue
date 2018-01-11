909b50897c2f7bf6c5f28b74cacc4f98fa0ec444
jiepi-zj <zj1991429>
2018-01-11 18:09:44 +0800

    设备管理模块，因为对应的数据库表没找到，所以只写了页面框架，请求链没有调通

--- src/pages/park/EquipmentManage_Channel.vue
+++ src/pages/park/EquipmentManage_Channel.vue
@@ -1,17 +1,185 @@
 <template>
-    <div>通道管理</div>
+    <section>
+        <common-table
+                :queryapi="queryapi"
+                :tableheight="tableheight"
+                :fieldsstr="fieldsstr"
+                :tableitems="tableitems"
+                :btswidth="btswidth"
+                :hide-export="hideExport"
+                :hide-options="hideOptions"
+                :addtitle="addtitle"
+                :searchtitle="searchtitle"
+                :showdateSelector="showdateSelector"
+                :hideTool="hideTool"
+                :showParkInfo="showParkInfo"
+                :hideSearch="hideSearch"
+                :hideAdd="hideAdd"
+                :showEdit="showEdit"
+                ref="bolinkuniontable"
+        ></common-table>
+    </section>
 </template>
 
 <script>
+
+    import {path, checkURL, checkUpload, checkNumber, channlManager,channlManagerType} from '../../api/api';
+    import util from '../../common/js/util'
+    import common from '../../common/js/common'
+    import CommonTable from '../../components/CommonTable'
+
     export default {
-        components: {},
+        components: {       //组件加载
+            CommonTable      //表格
+        },
         data() {
-            return {}
+            return {
+                loading: false,         //loading页面是否显示
+                hideExport: true,       //隐藏导出
+                hideSearch: true,      //隐藏查询
+                showdateSelector: true,//显示日期查询
+                hideAdd: false,         //隐藏添加
+                tableheight: '',        //表格高度
+                showdelete: true,       //显示删除
+                hideOptions: true,      //隐藏多选框
+                showParkInfo: false,     //显示停车信息
+                hideTool: false,        //隐藏工具栏
+                showEdit:true,
+                queryapi: '/EQ_channel/query',    //数据请求路径
+                addapi: '/EQ_channel/add',
+                editapi: '/EQ_channel/edit',
+                delapi: '/EQ_channel/remove',
+                btswidth: '100',                 //按钮宽度
+                fieldsstr: 'id__trade_no__card_id__pay_time__amount_receivable__amount_pay__collector__pay_type__car_number__user_id__limit_time__resume',//请求数据的格式，在云平台的页面找接口和有关请求参数。
+                tableitems: [                       //表格元素，表头
+                    {
+
+                        hasSubs: false,
+                        subs: [{
+                            label: '编号',          //页面表格显示
+                            prop: 'id',             //对应表中字段
+                            width: '100',           //列宽度
+                            type: 'number',         //对应表中字段类型
+                            editable: true,         //是否可编辑
+                            searchable: true,       //是否可查询
+                            addable: false,          //是否可添加
+                            unsortable: true,       //是否可排序
+                            align: 'center'         //页面表格内容显示位置
+                        }]
+                    }, {
+
+                        hasSubs: false,
+                        subs: [{
+                            label: '名称',
+                            prop: 'trade_no',
+                            width: '100',
+                            type: 'str',
+                            editable: true,
+                            searchable: true,
+                            addable: true,
+                            unsortable: true,
+                            align: 'center'
+                        }]
+                    }, {
+
+                        hasSubs: false,
+                        subs: [{
+                            label: '通道类型',
+                            prop: 'card_id',
+                            width: '150',
+                            type: 'selection',
+                            selectlist:channlManagerType,
+                            editable: true,
+                            searchable: true,
+                            addable: true,
+                            unsortable: true,
+                            align: 'center'
+                        }]
+                    }, {
+
+                        hasSubs: false,
+                        subs: [{
+                            label: '所属工作站',
+                            prop: 'pay_time',
+                            width: '190',
+                            type: 'selection',
+                            //selectlist:channlManager,选项为工作站管理页面的名称栏
+                            editable: true,
+                            searchable: true,
+                            addable: true,
+                            unsortable: true,
+                            align: 'center',
+                        }]
+                    }, {
+
+                        hasSubs: false,
+                        subs: [{
+                            label: '非月卡车',
+                            prop: 'amount_receivable',
+                            width: '150',
+                            type: 'selection',
+                            selectlist:channlManager,
+                            editable: true,
+                            searchable: true,
+                            addable: true,
+                            unsortable: true,
+                            align: 'center',
+                        }]
+                    }, {
+
+                        hasSubs: false,
+                        subs: [{
+                            label: '月卡第二辆车',
+                            prop: 'amount_pay',
+                            width: '150',
+                            type: 'selection',
+                            selectlist:channlManager,
+                            editable: false,
+                            searchable: true,
+                            addable: true,
+                            unsortable: true,
+                            align: 'center'
+                        }]
+                    },{
+
+                        hasSubs: false,
+                        subs: [{
+                            label: '说明',
+                            prop: 'amount_pay',
+                            width: '300',
+                            type: 'str',
+                            editable: false,
+                            searchable: true,
+                            addable: true,
+                            unsortable: true,
+                            align: 'center'
+                        }]
+                    },
+                ],
+
+                addtitle: '添加通道',
+            }
+        },
+        mounted() {
+            window.onresize = () => {
+                this.tableheight = common.gwh() - 143;
+            };
+            this.tableheight = common.gwh() - 143;
+        },
+        activated() {
+            window.onresize = () => {
+                this.tableheight = common.gwh() - 143;
+            };
+            this.tableheight = common.gwh() - 143;
+            this.$refs['bolinkuniontable'].$refs['search'].resetSearch();
+            this.$refs['bolinkuniontable'].getTableData({})
         },
         methods: {}
     }
 </script>
 
 <style scoped>
-
+    .gutter {
+        display: none
+    }
 </style>
--- src/pages/park/EquipmentManage_Intercom.vue
+++ src/pages/park/EquipmentManage_Intercom.vue
@@ -1,17 +1,169 @@
 <template>
-    <div>对讲管理</div>
+    <section>
+        <common-table
+                :queryapi="queryapi"
+                :tableheight="tableheight"
+                :fieldsstr="fieldsstr"
+                :tableitems="tableitems"
+                :btswidth="btswidth"
+                :hide-export="hideExport"
+                :hide-options="hideOptions"
+                :addtitle="addtitle"
+                :searchtitle="searchtitle"
+                :showdateSelector="showdateSelector"
+                :hideTool="hideTool"
+                :showParkInfo="showParkInfo"
+                :hideSearch="hideSearch"
+                :hideAdd="hideAdd"
+                :showEdit="showEdit"
+                ref="bolinkuniontable"
+        ></common-table>
+    </section>
 </template>
 
 <script>
+
+    import {path, checkURL, checkUpload, checkNumber, showType} from '../../api/api';
+    import util from '../../common/js/util'
+    import common from '../../common/js/common'
+    import CommonTable from '../../components/CommonTable'
+
     export default {
-        components: {},
+        components: {       //组件加载
+            CommonTable      //表格
+        },
         data() {
-            return {}
+            return {
+                loading: false,         //loading页面是否显示
+                hideExport: true,       //隐藏导出
+                hideSearch: false,      //隐藏查询
+                showdateSelector: true,//显示日期查询
+                hideAdd: false,         //隐藏添加
+                tableheight: '',        //表格高度
+                showdelete: true,       //显示删除
+                hideOptions: true,      //隐藏多选框
+                showParkInfo: false,     //显示停车信息
+                hideTool: false,        //隐藏工具栏
+                showEdit:true,
+                queryapi: '/EQ_intercom/query',    //数据请求路径
+                addapi: '/EQ_intercom/add',
+                editapi: '/EQ_intercom/edit',
+                delapi: '/EQ_intercom/remove',
+                btswidth: '100',                 //按钮宽度
+                fieldsstr: 'id__trade_no__card_id__pay_time__amount_receivable__amount_pay__collector__pay_type__car_number__user_id__limit_time__resume',//请求数据的格式，在云平台的页面找接口和有关请求参数。
+                tableitems: [                       //表格元素，表头
+                    {
+
+                        hasSubs: false,
+                        subs: [{
+                            label: '编号',          //页面表格显示
+                            prop: 'id',             //对应表中字段
+                            width: '100',           //列宽度
+                            type: 'number',         //对应表中字段类型
+                            editable: true,         //是否可编辑
+                            searchable: true,       //是否可查询
+                            addable: false,          //是否可添加
+                            unsortable: false,       //是否可排序
+                            align: 'center'         //页面表格内容显示位置
+                        }]
+                    }, {
+
+                        hasSubs: false,
+                        subs: [{
+                            label: '名称',
+                            prop: 'trade_no',
+                            width: '100',
+                            type: 'str',
+                            editable: true,
+                            searchable: true,
+                            addable: true,
+                            unsortable: false,
+                            align: 'center'
+                        }]
+                    }, {
+
+                        hasSubs: false,
+                        subs: [{
+                            label: '分机号',
+                            prop: 'card_id',
+                            width: '150',
+                            type: 'number',
+                            editable: true,
+                            searchable: true,
+                            addable: true,
+                            unsortable: false,
+                            align: 'center'
+                        }]
+                    }, {
+
+                        hasSubs: false,
+                        subs: [{
+                            label: '车场主机号',
+                            prop: 'pay_time',
+                            width: '190',
+                            type: 'number',
+                            editable: true,
+                            searchable: true,
+                            addable: true,
+                            unsortable: false,
+                            align: 'center',
+                        }]
+                    }, {
+
+                        hasSubs: false,
+                        subs: [{
+                            label: '集团主机号',
+                            prop: 'amount_receivable',
+                            width: '150',
+                            type: 'str',
+                            editable: true,
+                            searchable: true,
+                            addable: true,
+                            unsortable: false,
+                            align: 'center',
+                        }]
+                    }, {
+
+                        hasSubs: false,
+                        subs: [{
+                            label: '监控名称',
+                            prop: 'amount_pay',
+                            width: '150',
+                            type: 'selection',
+                            //selectlist:showType,//该处引用监控管理中的名称作为下拉选项
+                            editable: false,
+                            searchable: true,
+                            addable: true,
+                            unsortable: false,
+                            align: 'center'
+                        }]
+                    },
+                ],
+
+                addtitle: '添加对讲',
+                searchtitle: '搜索对讲',
+            }
+        },
+        mounted() {
+            window.onresize = () => {
+                this.tableheight = common.gwh() - 143;
+            };
+            this.tableheight = common.gwh() - 143;
+        },
+        activated() {
+            window.onresize = () => {
+                this.tableheight = common.gwh() - 143;
+            };
+            this.tableheight = common.gwh() - 143;
+            this.$refs['bolinkuniontable'].$refs['search'].resetSearch();
+            this.$refs['bolinkuniontable'].getTableData({})
         },
         methods: {}
     }
 </script>
 
 <style scoped>
-
+    .gutter {
+        display: none
+    }
 </style>
