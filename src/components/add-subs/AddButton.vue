f6f83c969e1068812d7d3e71745ebbe66fc41490
xulu <1510312433@qq.com>
2018-01-29 16:18:55 +0800

    移除lock.json

---
+++ src/components/add-subs/AddButton.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="number" style="margin-right: 15px">
+        <!--<el-input v-model="textForm.text" @blur="uptoeditdialog"></el-input>-->
+
+    <el-button type="primary"  @click="itemclick">
+        {{textForm.text}}
+    </el-button>
+    </div>
+</template>
+
+<script>
+    import common from '../../common/js/common.js'
+    export default {
+        name: 'buttons',
+        data () {
+            return {
+                textForm:{
+                    text:''
+                },
+                tempForm:{
+                    text:''
+                },
+                upForm:{}
+            }
+        },
+        props:['id'],
+
+        methods:{
+            uptoeditdialog:function(){
+                // this.upForm[this.id]=String.trim(this.textForm.text)
+                this.upForm[this.id]=this.textForm.text.trim()
+                this.$emit('fromedititem',this.upForm)
+            },
+            setValue:function(){
+                this.textForm=common.clone(this.tempForm)
+                this.upForm={}
+            },
+            itemclick:function () {
+                console.log(' button click ')
+            }
+        },
+
+    }
+</script>
