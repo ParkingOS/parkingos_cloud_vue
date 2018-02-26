854773e3c5735102ad436b8f95fa9d7909247138
xulu <1510312433@qq.com>
2018-02-26 18:26:44 +0800

    修改日报时间格式

--- src/components/CommonTable.vue
+++ src/components/CommonTable.vue
@@ -56,7 +56,7 @@
                                 range-separator="至"
                                 start-placeholder="开始日期"
                                 end-placeholder="结束日期"
-                                value-format="yyyy-MM-dd"
+                                value-format="yyyy-MM-dd HH:mm:ss"
                                 :picker-options="pickerOptions2"
                                 @change="changeanalysisdate">
                         </el-date-picker>
