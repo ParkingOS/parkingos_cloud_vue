856087a32a48f7a2fe1de81fb4c1c0cc02ecf238
xulu <1510312433@qq.com>
2018-03-08 09:05:24 +0800

    集团会员模块

--- src/api/api.js
+++ src/api/api.js
@@ -1,37 +1,37 @@
-// export const path = "http://120.25.121.204:8080/cloud"
+export const path = 'http://120.25.121.204:8080/cloud';
 
 // export const path = "http://localhost:8086"
-export const path = "http://192.168.199.205:12305"
+// export const path = 'http://192.168.199.205:12305';
 //export const path = "http://localhost:8085"
 // export const path = "http://localhost:8080/cloud"
 
 
-export const phonewidth = '150'
+export const phonewidth = '150';
 //beta设置为5   s.bolink设置为2 
-export const percision = 5
+export const percision = 5;
 
 
 export const operateType = [
     {'value_no': 0, 'value_name': '全部'},
     {'value_no': 1, 'value_name': '续费'},
     {'value_no': 2, 'value_name': '回收充值'}
-]
+];
 export const ticketType = [
     {'value_name': '时长减免', 'value_no': '1'},
     {'value_name': '金额减免', 'value_no': '2'}
-]
+];
 export const ticketQueryType = [
     {'value_no': 0, 'value_name': '全部'},
     {'value_no': 1, 'value_name': '时长减免'},
     {'value_no': 2, 'value_name': '金额减免'}
-]
+];
 
 export const statType = [
     {'value_no': -1, 'value_name': '全部'},
     {'value_no': 0, 'value_name': '未使用'},
     {'value_no': 1, 'value_name': '已使用'},
     {'value_no': 2, 'value_name': '回收作废'}
-]
+];
 export const moveStyle = [
     {'value_no': 0, 'value_name': '自适应'},
     {'value_no': 1, 'value_name': '从右向左移动'},
@@ -49,8 +49,8 @@ export const moveStyle = [
     {'value_no': 13, 'value_name': '从上下向中间展开'},
     {'value_no': 14, 'value_name': '闪烁'},
     {'value_no': 15, 'value_name': '右百叶'}
-    ,
-]
+    
+];
 
 export const moveSpeed = [
     {'value_no': 0, 'value_name': '0'},
@@ -62,7 +62,7 @@ export const moveSpeed = [
     {'value_no': 6, 'value_name': '6'},
     {'value_no': 7, 'value_name': '7'},
     {'value_no': 8, 'value_name': '8'}
-]
+];
 
 export const waitingTime = [
     {'value_no': 0, 'value_name': '0s'},
@@ -71,23 +71,23 @@ export const waitingTime = [
     {'value_no': 3, 'value_name': '3s'},
     {'value_no': 4, 'value_name': '4s'},
     {'value_no': 5, 'value_name': '5s'}
-]
+];
 
 export const LEDStyle = [
     {'value_no': 0, 'value_name': '普通屏'},
     {'value_no': 1, 'value_name': '余位屏'}
-]
+];
 
 export const redirectPort = [
     {'value_no': 0, 'value_name': 'rs232-1'},
     {'value_no': 1, 'value_name': 'rs232-2'},
     {'value_no': 2, 'value_name': 'rs485'}
-]
+];
 
 export const LEDColor = [
     {'value_no': 0, 'value_name': '单基色'},
     {'value_no': 1, 'value_name': '双基色'}
-]
+];
 
 export const fontSize = [
     {'value_no': 0, 'value_name': '12×12'},
@@ -98,7 +98,7 @@ export const fontSize = [
     {'value_no': 5, 'value_name': '64×64'},
     {'value_no': 6, 'value_name': '80×80'},
     {'value_no': 7, 'value_name': '96×96'}
-]
+];
 
 export const fontStyle = [
     {'value_no': 0, 'value_name': '宋体'},
@@ -106,46 +106,46 @@ export const fontStyle = [
     {'value_no': 2, 'value_name': '黑体'},
     {'value_no': 3, 'value_name': '隶书'},
     {'value_no': 4, 'value_name': '行书'}
-]
+];
 
 export const color = [
     {'value_no': 0, 'value_name': '红'},
     {'value_no': 1, 'value_name': '绿'},
     {'value_no': 2, 'value_name': '黄'}
 
-]
+];
 
 export const channlManager = [
     {'value_name': '默认车场设置', 'value_no': '0'},
     {'value_name': '不限制', 'value_no': '1'},
     {'value_name': '禁止进入', 'value_no': '2'}
-]
+];
 
 export const channlManagerType = [
     {'value_name': '出', 'value_no': '0'},
     {'value_name': '入', 'value_no': '1'},
     {'value_name': '出人', 'value_no': '2'}
-]
+];
 
 export const net_status = [
     {'value_name': '异常', 'value_no': '0'},
     {'value_name': '正常', 'value_no': '1'}
-]
+];
 
 export const monitorType = [
     {'value_name': '隐藏', 'value_no': '0'},
     {'value_name': '显示', 'value_no': '1'}
-]
+];
 
 export const workStation = [
     {'value_no': 0, 'value_name': '流量'},
     {'value_no': 1, 'value_name': '宽带'}
-]
+];
 
 export const payType = [
     {'value_no': 0, 'value_name': '现金'},
     {'value_no': 1, 'value_name': '电子支付'}
-]
+];
 export const orderPayType = [
     //0:帐户支付,1:现金支付,2:手机支付 3:包月 4:现金预支付 5：银联卡(中央预支付，后面废弃) 6：商家卡(中央预支付，后面废弃) 8：免费放行 9：刷卡
     // {'value_name': '账户支付', 'value_no': 0},
@@ -156,132 +156,133 @@ export const orderPayType = [
     // {'value_name': '现金预支付', 'value_no': 4,},
     // {'value_name': '银联卡支付', 'value_no': 5,},
     // {'value_name': '商家卡支付', 'value_no': 6,},
-    {'value_name': '免费放行', 'value_no': 8,},
+    {'value_name': '免费放行', 'value_no': 8}
     // {'value_name': '刷卡', 'value_no': 9}
-]
+];
 export const RoleFuncion = [
     {'value_no': 0, 'value_name': '无'},
     {'value_no': 1, 'value_name': '收费功能'},
     {'value_no': 2, 'value_name': '巡查功能'},
     {'value_no': 3, 'value_name': '开卡功能'}
-    ,
-]
+    
+];
 //文字放前面，编辑的时候显示文字。否则显示编号
 export const genderType = [
-    {'value_name': '未设置', 'value_no': '-1',},
-    {'value_name': '女', 'value_no': '0',},
-    {'value_name': '男', 'value_no': '1',}
-]
+    {'value_name': '未设置', 'value_no': '-1'},
+    {'value_name': '女', 'value_no': '0'},
+    {'value_name': '男', 'value_no': '1'}
+];
 export const collectType = [
     {'value_name': '未设置', 'value_no': '-1'},
     {'value_name': '不可收费', 'value_no': '0'},
     {'value_name': '可收费', 'value_no': '1'}
-]
+];
 export const blackStateType = [
     {'value_name': '正常', 'value_no': '0'},
     {'value_name': '漂白', 'value_no': '1'}
-]
+];
 export const commuteStateType = [
     {'value_name': '已签到', 'value_no': '0'},
     {'value_name': '已签退', 'value_no': '1'}
-]
+];
 export const singleDoubleType = [
     {'value_name': '不限制', 'value_no': '0'},
     {'value_name': '限制', 'value_no': '1'}
-]
+];
 export const orderStateType = [
     {'value_name': '未结算', 'value_no': '0'},
-    {'value_name': '已结算', 'value_no': '1'},
+    {'value_name': '已结算', 'value_no': '1'}
     // {'value_name': '逃单', 'value_no': '2'}
-]
+];
 /**
  * 集团固定类型
  * @type {*[]}
  */
 export const inparkType = [
     //进场方式
-    {"value_name": "全部", "value_no": "-1"},
-    {"value_name": "通道扫牌", "value_no": ""},
-    {"value_name": "NFC刷卡", "value_no": "0"},
-    {"value_name": "Ibeacon", "value_no": "1"},
-    {"value_name": "手机扫牌", "value_no": "2"},
-    {"value_name": "通道扫牌", "value_no": "3"},
-    {"value_name": "直付", "value_no": "4"},
-    {"value_name": "月卡", "value_no": "5"},
-    {"value_name": "车位二维码", "value_no": "6"},
-    {"value_name": "月卡第二辆车", "value_no": "7"}
-]
+    {'value_name': '全部', 'value_no': '-1'},
+    {'value_name': '通道扫牌', 'value_no': ''},
+    {'value_name': 'NFC刷卡', 'value_no': '0'},
+    {'value_name': 'Ibeacon', 'value_no': '1'},
+    {'value_name': '手机扫牌', 'value_no': '2'},
+    {'value_name': '通道扫牌', 'value_no': '3'},
+    {'value_name': '直付', 'value_no': '4'},
+    {'value_name': '月卡', 'value_no': '5'},
+    {'value_name': '车位二维码', 'value_no': '6'},
+    {'value_name': '月卡第二辆车', 'value_no': '7'}
+];
 export const parkType = [
     //车场类型
-    {"value_name": "全部", "value_no": -1},
-    {"value_name": "地面", "value_no": 0},
-    {"value_name": "地下", "value_no": 1},
-    {"value_name": "占道", "value_no": 2}
-]
+    {'value_name': '全部', 'value_no': -1},
+    {'value_name': '地面', 'value_no': 0},
+    {'value_name': '地下', 'value_no': 1},
+    {'value_name': '占道', 'value_no': 2}
+];
 
-export var checkPhone = (rule, value, callback) => {
-    if (typeof(value) == 'undefined' || value == '') {
-        return callback(new Error('请输入正确的电话号码'))
-    } else if (!((/^800[0-9]{7}$/.test(value)) || (/^400[0-9]{7}$/.test(value)) || (/^1[34578]\d{9}$/.test(value)) || (/^(0[0-9]{2,3}\-)([2-9][0-9]{6,7})$/.test(value)))) {
-        return callback(new Error('请输入手机,座机(区号后加-),或400,800开头号码'))
-    } else {
-        callback()
-    }
-}
+// export var checkPhone = (rule, value, callback) => {
+//     if (typeof(value) == 'undefined' || value == '') {
+//         return callback(new Error('请输入正确的电话号码'));
+//     } else if (!((/^800[0-9]{7}$/.test(value)) || (/^400[0-9]{7}$/.test(value)) || (/^1[34578]\d{9}$/.test(value)) || (/^(0[0-9]{2,3}\-)([2-9][0-9]{6,7})$/.test(value)))) {
+//         return callback(new Error('请输入手机,座机(区号后加-),或400,800开头号码'));
+//     } else {
+//         callback();
+//     }
+// };
 
 export var checkMobile = (rule, value, callback) => {
     if (typeof(value) == 'undefined' || value == '') {
-        return callback(new Error('请输入正确的手机号码'))
+        return callback(new Error('请输入正确的手机号码'));
     } else if (!((/^1[34578]\d{9}$/.test(value)))) {
-        return callback(new Error('请输入正确的手机号码'))
+        return callback(new Error('请输入正确的手机号码'));
     } else {
-        callback()
+        callback();
     }
-}
+};
 
 export var checkURL = (rule, value, callback) => {
-    if (typeof(value) == 'undefined' || value == '') {
-        return callback(new Error('请输入地址'))
-    } else if (!(/(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?/.test(value))) {
-        return callback(new Error('请输入正确的地址'))
-    } else {
-        callback()
-    }
-}
+    // if (typeof(value) == 'undefined' || value == '') {
+    //     return callback(new Error('请输入地址'));
+    // } else if (!(/(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?/.test(value))) {
+    //     return callback(new Error('请输入正确的地址'));
+    // } else {
+    //     callback();
+    // }
+    console.log(''+callback);
+};
 
 export var checkUpload = (rule, value, callback) => {
     if (typeof(value) == 'undefined' || value == '') {
-        return callback(new Error('请上传图片'))
+        return callback(new Error('请上传图片'));
     } else {
-        return callback()
+        return callback();
     }
-}
+};
 
 export var checkNumber = (rule, value, callback) => {
     if (typeof(value) == 'undefined' || value == '') {
-        return callback(new Error('请输入权重'))
+        return callback(new Error('请输入权重'));
     } else if (!(/^[0-9]{1,2}$/.test(value)) || value == 0) {
-        return callback(new Error('权重值在1-99'))
+        return callback(new Error('权重值在1-99'));
     } else {
-        callback()
+        callback();
     }
-}
+};
 
 export var checkPass = (rule, value, callback) => {
     if (!(/^(\w){6,12}$/.test(value))) {
-        return callback(new Error('密码为6-12位字母,数字或下划线'))
+        return callback(new Error('密码为6-12位字母,数字或下划线'));
     } else {
-        callback()
+        callback();
     }
-}
+};
 
 export var checkCityInfo = (rule, value, callback) => {
-    console.log("1111=>" + value)
+    console.log('1111=>' + value);
     if (typeof(value) == 'undefined' || value == '') {
-        return callback(new Error('请选择城市信息'))
+        return callback(new Error('请选择城市信息'));
     } else {
-        callback()
+        callback();
     }
-}
+};
 
 //export const distinctslist = () => { return axios.get('http://jarvisqh.vicp.io/cms-web/getdata/getdistinctlist') }
