e1e2ba5fc0d93371207984f609de5027eae6a2da
xulu <1510312433@qq.com>
2018-03-15 09:17:01 +0800

    集团页面完成

--- src/pages/union/StrategicAnalysis_DailyReport.vue
+++ src/pages/union/StrategicAnalysis_DailyReport.vue
@@ -73,7 +73,7 @@
                                 addable: true,
                                 unsortable: true,
                                 align: 'center',
-                               
+
                             },
                         ]
                     },
