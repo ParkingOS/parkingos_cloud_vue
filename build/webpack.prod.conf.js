0ca8fec5708b258a823733d090f266aa77802a71
chenbw <724791965@qq.com>
2018-04-02 20:40:09 +0800

    缺少webpackjson,登录不上

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -40,6 +40,26 @@ var webpackConfig = merge(baseWebpackConfig, {
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
     // see https://github.com/ampedandwired/html-webpack-plugin
+      new webpack.optimize.CommonsChunkPlugin({
+          name: 'vendor',
+          minChunks: function (module, count) {
+              // any required modules inside node_modules are extracted to vendor
+              return (
+                  module.resource &&
+                  /\.js$/.test(module.resource) &&
+                  module.resource.indexOf(
+                      path.join(__dirname, '../node_modules')
+                  ) === 0
+              )
+          }
+      }),
+      // extract webpack runtime and module manifest to its own file in order to
+      // prevent vendor hash from being updated whenever app bundle is updated
+      new webpack.optimize.CommonsChunkPlugin({
+          name: 'manifest',
+          chunks: ['vendor']
+      }),
+
     new HtmlWebpackPlugin({
       filename: config.build.index,
       template: 'index.html',
@@ -53,58 +73,41 @@ var webpackConfig = merge(baseWebpackConfig, {
       },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
       chunksSortMode: 'dependency',
-       chunks : ['app'],
+       //chunks : ['app'],
+        chunks: ['manifest', 'vendor', 'app']
     }),
-    new HtmlWebpackPlugin({
-      filename: 'monitor.html',
-      template: 'monitor.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true
-        // more options:
-        // https://github.com/kangax/html-minifier#options-quick-reference
-      },
-      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: 'dependency',
-      chunks : ['monitor'],
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'devicewin.html',
-      template: 'devicewin.html',
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeAttributeQuotes: true
-        // more options:
-        // https://github.com/kangax/html-minifier#options-quick-reference
-      },
-      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: 'dependency',
-      chunks : ['devicewin'],
-    }),
+      new HtmlWebpackPlugin({
+          filename: 'monitor.html',
+          template: 'monitor.html',
+          inject: true,
+          minify: {
+              removeComments: true,
+              collapseWhitespace: true,
+              removeAttributeQuotes: true
+              // more options:
+              // https://github.com/kangax/html-minifier#options-quick-reference
+          },
+          // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+          chunksSortMode: 'dependency',
+          chunks : ['monitor'],
+      }),
+      new HtmlWebpackPlugin({
+          filename: 'devicewin.html',
+          template: 'devicewin.html',
+          inject: true,
+          minify: {
+              removeComments: true,
+              collapseWhitespace: true,
+              removeAttributeQuotes: true
+              // more options:
+              // https://github.com/kangax/html-minifier#options-quick-reference
+          },
+          // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+          chunksSortMode: 'dependency',
+          chunks : ['devicewin'],
+      })
+
     // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module, count) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
-      }
-    }),
-    // extract webpack runtime and module manifest to its own file in order to
-    // prevent vendor hash from being updated whenever app bundle is updated
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      chunks: ['vendor']
-    })
   ]
 })
 
