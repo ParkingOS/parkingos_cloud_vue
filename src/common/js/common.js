14c7f4b54d83c9441db2b56976db37dd65592451
Hunmanlife <562953415@qq.com>
2018-06-06 16:13:24 +0800

    时间格式化

--- src/common/js/common.js
+++ src/common/js/common.js
@@ -548,6 +548,16 @@ export default {
         let threeDaysAgo = new Date(new Date().getTime()-(delay-1)*24*60*60*1000);
         let tda = threeDaysAgo.getFullYear() + '-' + this.formatNumber(threeDaysAgo.getMonth() + 1) + '-' + this.formatNumber(threeDaysAgo.getDate());
         return [tda+' 00:00:00',this.currentDate()+' 23:59:59'];
+    },
+    timestampToTime(timestamp){
+        var date,Y,M,D,h,m,s;
+        var date = new Date(timestamp*1000 );//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+        Y = date.getFullYear() + '-';
+        M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+        D = date.getDate() + ' ';
+        h = date.getHours() + ':';
+        m = date.getMinutes() + ':';
+        s = date.getSeconds();
+        return Y+M+D+h+m+s;
     }
-
 };
