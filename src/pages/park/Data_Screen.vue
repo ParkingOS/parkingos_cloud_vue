ef89fd2902e81c5a8691ddb7dfd830e66179ff5a
Hunmanlife <562953415@qq.com>
2018-12-19 16:55:07 +0800

    data-screen

---
+++ src/pages/park/Data_Screen.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="data-screen-wrapper">
+        <header class="header" :style="headerStyle"></header>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'data_-screen',
+        data(){
+            let that = this;
+            return {
+                screenBg:require('@/assets/images/screen/screenBg.png'),
+                headerBg:require('@/assets/images/screen/headerBg.png'),
+                headerStyle:{
+                    background:'url('+require('@/assets/images/screen/headerBg.png')+') no-repeat',
+                    backgroundSize:'100% 100%'
+                },
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .data-screen-wrapper{
+        position: absolute;
+        top:0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: rgba(4,21,53,1);
+        .header{
+            padding: 0;
+            margin: 0;
+            height: 59px;
+        }
+    }
+</style>
