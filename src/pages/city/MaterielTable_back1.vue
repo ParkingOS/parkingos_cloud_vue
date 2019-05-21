db07eae11cfb03b3108cd916e7bc1d8ebfbc1760
Hunmanlife <562953415@qqcom>
2019-05-21 11:07:51 +0800

    backups

---
+++ src/pages/city/MaterielTable_back1.vue
@@ -0,0 +1,29 @@
+<template>
+    <section :style="{width: '100%',height:domHeight+'px'}" class="bolink-wrapper">
+        <iframe width="100%" height="100%" src="http://localhost:8082/#/materiel" frameborder="0"></iframe>
+    </section>
+</template>
+<script>
+    import common from '../../common/js/common'
+    export default {
+        data(){
+            return {
+                domHeight:500,
+            }
+        },
+        mounted(){
+            let that = this;
+            this.domHeight = common.gwh() - 90;
+            window.addEventListener('resize', () => {
+                that.domHeight = common.gwh() - 90;
+            });
+        }
+    }
+</script>
+<style scoped>
+    .bolink-wrapper{
+        display: block;
+        margin-top: 20px;
+    }
+</style>
+
